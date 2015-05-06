e2ede70149af32b1f7990575abee22a02189e9dd
Gede Lumbung <gedesumawijaya@gmail.com>
2015-05-06 15:26:25 +0800

    fix error link and session

--- protected/views/site/error.php
+++ protected/views/site/error.php
@@ -1,5 +1,5 @@
 
 <div class="hero-unit">
-	<h2>Error - <?php echo $GLOBALS['site_title']; ?> Supporting Ticket</h2>
+	<h2>Error - <?php echo $_SESSION['site_title']; ?> Supporting Ticket</h2>
 	<?php echo CHtml::encode($message); ?>
 </div>
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,5 +1,5 @@
 <div class="hero-unit">
 	<h2>Welcome to, <?php echo $_SESSION['site_title']; ?></h2>
 	<?php echo $_SESSION['site_welcome']; ?>
-	<p><a class="btn btn-primary btn-large" href="<?php echo Yii::app()->baseUrl; ?>/login">Administrator Login <i class="icon-circle-arrow-right icon-white"></i> </a></p>
+	<p><a class="btn btn-primary btn-large" href="<?php echo Yii::app()->baseUrl; ?>/site/login">Administrator Login <i class="icon-circle-arrow-right icon-white"></i> </a></p>
 </div>
